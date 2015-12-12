c1b10a6f72e37f7c30e35490c68bcf9da97aab18
Mandar <01.mandar@gmail.com>
2015-12-12 22:44:34 +0530

    basic add matrix cases

--- 2_1_Matrix_Addition/addMatrix.cu
+++ 2_1_Matrix_Addition/addMatrix.cu
@@ -14,21 +14,49 @@
 #include "device_launch_parameters.h"
 
 #include <stdio.h>
+#include <memory>
 
+enum MATRIX_TYPE{ MATRIX_INITIALIZE, MATRIX_RANDOM, MATRIX_IDENTITY };
 
 __global__  void addMatrix(float *array1, float *array2, float *result, int WIDTH)
 {
 
 }
 
-void prepareMatrix()
+float* prepareSquareMatrix(int isz, MATRIX_TYPE typ)
 {
+	int matSize = isz*isz;
+	float *mat = (float*)malloc(matSize* sizeof(float));
 
+	switch (typ)
+	{
+	case MATRIX_INITIALIZE:
+		memset(mat, 0, matSize * sizeof(float));
+		break;
+	case MATRIX_RANDOM:
+		for (int j = 0; j<(matSize); j++)
+			mat[j] = (float)rand() / (float)RAND_MAX;
+		break;
+	case MATRIX_IDENTITY:
+		// NOT IN USE
+		break;
+	}
+
+	return mat;
 }
 
 
 int main()
 {
-    
+	// Pointer for matrix
+	float *A, *B, *SUM;
+	
+	// Initializing matrix with data
+	A = prepareSquareMatrix(4,MATRIX_RANDOM);  // 4 X 4
+	B = prepareSquareMatrix(4, MATRIX_RANDOM);  // 4 X 4
+	SUM = prepareSquareMatrix(4, MATRIX_INITIALIZE);  // 4 X 4
+
+
+
     return 0;
 }
