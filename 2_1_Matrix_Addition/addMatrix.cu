691f14c89407661729f45b4f851d5a3aa9d75574
Mandar <01.mandar@gmail.com>
2015-12-13 22:46:12 +0530

    -not a valid output

--- 2_1_Matrix_Addition/addMatrix.cu
+++ 2_1_Matrix_Addition/addMatrix.cu
@@ -18,9 +18,26 @@
 
 enum MATRIX_TYPE{ MATRIX_INITIALIZE, MATRIX_RANDOM, MATRIX_IDENTITY };
 
-__global__  void addMatrix(float *array1, float *array2, float *result, int WIDTH)
+void CUDA_SAFE_CALL(cudaError_t call)
 {
+	cudaError_t ret = call;
+	if (ret != cudaSuccess)
+	{
+		printf(" ERROR at line :%i.%d' ' %s\n", __LINE__, ret, cudaGetErrorString(ret));
+		exit(-1);
+	}
+}
 
+__global__  void GPUAdd(float *array1, float *array2, float *result, int WIDTH)
+{
+	int i = blockDim.x * blockIdx.x + threadIdx.x;
+	result[i] = array1[i] + array2[i];
+}
+
+void CPUAdd(float *array1, float *array2, float *result, int WIDTH)
+{
+	for (int i = 0; i < WIDTH; i++)
+		result[i] = array1[i] + array2[i];
 }
 
 float* prepareSquareMatrix(int isz, MATRIX_TYPE typ)
@@ -34,29 +51,56 @@ float* prepareSquareMatrix(int isz, MATRIX_TYPE typ)
 		memset(mat, 0, matSize * sizeof(float));
 		break;
 	case MATRIX_RANDOM:
-		for (int j = 0; j<(matSize); j++)
+		for (long j = 0; j<(matSize); j++)
 			mat[j] = (float)rand() / (float)RAND_MAX;
 		break;
 	case MATRIX_IDENTITY:
 		// NOT IN USE
 		break;
 	}
-
 	return mat;
 }
+
 
 
 int main()
 {
 	// Pointer for matrix
-	float *A, *B, *SUM;
+	float *A, *B, *SUM;				// HOST
+	float *cudaA, *cudaB, *cudaSUM, *cudaRET;	// DEVICE
+	int msize = 1000;
 	
 	// Initializing matrix with data
-	A = prepareSquareMatrix(4,MATRIX_RANDOM);  // 4 X 4
-	B = prepareSquareMatrix(4, MATRIX_RANDOM);  // 4 X 4
-	SUM = prepareSquareMatrix(4, MATRIX_INITIALIZE);  // 4 X 4
+	A = prepareSquareMatrix(msize, MATRIX_RANDOM);  // 4 X 4
+	B = prepareSquareMatrix(msize, MATRIX_RANDOM);  // 4 X 4
+	SUM = prepareSquareMatrix(msize, MATRIX_INITIALIZE);  // 4 X 4
+	cudaRET = (float *)malloc(msize*msize);
+
+	CPUAdd(A, B, SUM, msize*msize);
+
+	CUDA_SAFE_CALL(cudaMalloc((void **)&cudaA, msize*msize*sizeof(float)));
+	CUDA_SAFE_CALL(cudaMalloc((void **)&cudaB, msize*msize*sizeof(float)));
+	CUDA_SAFE_CALL(cudaMalloc((void **)&cudaSUM, msize*msize*sizeof(float)));
 
 
+	int threadsPerBlock = 256;
+	int blocksPerGrid = (msize + threadsPerBlock - 1) / threadsPerBlock;
+	printf("\nBlocks per Grid :%d\nThreads pre Block :%d\n", blocksPerGrid, threadsPerBlock);
+
+	CUDA_SAFE_CALL(cudaMemcpy(cudaA, A, msize*msize*sizeof(float), cudaMemcpyHostToDevice));
+	CUDA_SAFE_CALL(cudaMemcpy(cudaB, B, msize*msize*sizeof(float), cudaMemcpyHostToDevice));
+	GPUAdd << <blocksPerGrid, threadsPerBlock >> >(cudaA, cudaB, cudaSUM, msize*msize);
+	CUDA_SAFE_CALL(cudaMemcpy(cudaRET, cudaSUM, msize*msize, cudaMemcpyDeviceToHost));
+	
+	for (int i = 0; i < msize*msize; i++)
+		printf(" %.2f - %.2f  \n", cudaRET[i], SUM[i]);
+
+	cudaFree(cudaA);
+	cudaFree(cudaB);
+	cudaFree(cudaSUM);
+	free(A);
+	free(B);
+	free(SUM);
 
     return 0;
 }
