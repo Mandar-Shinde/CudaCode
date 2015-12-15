54139e52a5c638fc5d955eac8664bdc7de16a5d9
Mandar <01.mandar@gmail.com>
2015-12-15 23:18:40 +0530

    adding matrix complete

--- 2_1_Matrix_Addition/addMatrix.cu
+++ 2_1_Matrix_Addition/addMatrix.cu
@@ -4,7 +4,7 @@
 /////////////////////////////////////////////////////
 ///  COMPILER OPTIONS
 ///
-///
+/// C:\Users\mandar\Documents\GitHub\CudaCode\2_1_Matrix_Addition>"C:\Program Files\NVIDIA GPU Computing Toolkit\CUDA\v7.0\bin\nvcc.exe" -ccbin "C:\Program Files (x86)\Microsoft Visual Studio 12.0\VC\bin"  -I"C:\Program Files\NVIDIA GPU Computing Toolkit\CUDA\v7.0\include" -I"C:\Program Files\NVIDIA GPU Computing Toolkit\CUDA\v7.0\include"  -G   --keep-dir Debug -maxrregcount=0  --machine 32 --compile   -g   -DWIN32 -D_DEBUG -D_CONSOLE -D_MBCS -Xcompiler "/EHsc /W3 /nologo /Od /Zi /RTC1 /MDd  " -o Debug\addMatrix.cu.obj "C:\Users\mandar\Documents\GitHub\CudaCode\2_1_Matrix_Addition\addMatrix.cu" -clean   addMatrix.cu
 /////////////////////////////////////////////////////
 ///  OUTPUT
 ///
@@ -31,7 +31,7 @@ void CUDA_SAFE_CALL(cudaError_t call)
 __global__  void GPUAdd(float *array1, float *array2, float *result, int WIDTH)
 {
 	int i = blockDim.x * blockIdx.x + threadIdx.x;
-	result[threadIdx.x] = array1[threadIdx.x] + array2[threadIdx.x];
+	result[i] = array1[i] + array2[i];
 }
 
 void CPUAdd(float *array1, float *array2, float *result, int WIDTH)
@@ -75,6 +75,7 @@ int main()
 	B = prepareSquareMatrix(msize, MATRIX_RANDOM);  // 4 X 4
 	SUM = prepareSquareMatrix(msize, MATRIX_INITIALIZE);  // 4 X 4
 	cudaRET = (float *)malloc(msize*msize*sizeof(float));
+	memset(cudaRET, 0, msize*msize*sizeof(float));
 
 	CPUAdd(A, B, SUM, msize*msize);
 
@@ -83,15 +84,15 @@ int main()
 	CUDA_SAFE_CALL(cudaMalloc((void **)&cudaSUM, msize*msize*sizeof(float)));
 
 
-	int threadsPerBlock = 256;
-	int blocksPerGrid = (msize + threadsPerBlock - 1) / threadsPerBlock;
+	int threadsPerBlock = 32;
+	int blocksPerGrid = (msize*msize + threadsPerBlock - 1) / threadsPerBlock;
 	printf("\nBlocks per Grid :%d\nThreads pre Block :%d\n", blocksPerGrid, threadsPerBlock);
 
 	CUDA_SAFE_CALL(cudaMemcpy(cudaA, A, msize*msize*sizeof(float), cudaMemcpyHostToDevice));
 	CUDA_SAFE_CALL(cudaMemcpy(cudaB, B, msize*msize*sizeof(float), cudaMemcpyHostToDevice));
 	GPUAdd << <blocksPerGrid, threadsPerBlock >> >(cudaA, cudaB, cudaSUM, msize*msize);
-	CUDA_SAFE_CALL(cudaMemcpy(cudaRET, cudaSUM, msize*msize, cudaMemcpyDeviceToHost));
-	
+	CUDA_SAFE_CALL(cudaMemcpy(cudaRET, cudaSUM, msize*msize*sizeof(float), cudaMemcpyDeviceToHost));
+
 	cudaFree(cudaA);
 	cudaFree(cudaB);
 	cudaFree(cudaSUM);
