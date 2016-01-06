bbc8af23c70aae6515c2d3c2f129cc715b49f20f
Mandar <01.mandar@gmail.com>
2016-01-06 23:12:12 +0530

    stuck at 1d to 2d conversion

--- 2_2_Matrix_Multiplication/multMatrix.cu
+++ 2_2_Matrix_Multiplication/multMatrix.cu
@@ -1,7 +1,7 @@
 
 
 // some refrence from http://www.umiacs.umd.edu/~ramani/cmsc828e_gpusci/Lecture5.pdf
-
+//http://cs.nyu.edu/courses/fall14/CSCI-GA.3033-004/lecture3.pdf
 #include "cuda_runtime.h"
 #include "device_launch_parameters.h"
 
@@ -20,11 +20,15 @@ void CUDA_SAFE_CALL(cudaError_t call)
 	}
 }
 
-__global__  void GPUMult(float *A, float *B, float *C, int WIDTH)
+__global__  void GPUMult(int *A, int *B, int *C, int WIDTH)
 {	
-	float sol=0;
+	int sol=0;
+	int i;i = threadIdx.x;
+	int j; j= threadIdx.y;
 	for (int cnt = 0; cnt < WIDTH;cnt++)
 	{
+		if (i != cnt)
+			break;
 		// 1D to 2D calc
 		int h = WIDTH* cnt+ threadIdx.x; 
 		//  |
@@ -34,27 +38,34 @@ __global__  void GPUMult(float *A, float *B, float *C, int WIDTH)
 		int w = WIDTH * threadIdx.y + cnt; //->-->--> move in x (loop for Y constant)
 
 		sol += A[h]*B[w];
+		//printf("[%d]  [%d][%d]   [%d]*[%d] ==%d\n",cnt, h, w, A[h], B[w], sol);
 	}
+	printf(" <> %d, %d    \n", i,j );
 	C[i] = sol;
+
 }
 
-void CPUMult(float *array1, float *array2, float *result, int WIDTH)
+void CPUMult(int *array1, int *array2, int *result, int WIDTH)
 {
 }
 
-float* prepareSquareMatrix(int isz, MATRIX_TYPE typ)
+int* prepareSquareMatrix(int isz, MATRIX_TYPE typ)
 {
 	int matSize = isz*isz;
-	float *mat = (float*)malloc(matSize* sizeof(float));
+	int *mat = (int*)malloc(matSize* sizeof(int));
 
 	switch (typ)
 	{
 	case MATRIX_INITIALIZE:
-		memset(mat, 0, matSize * sizeof(float));
+		memset(mat, 0, matSize * sizeof(int));
 		break;
 	case MATRIX_RANDOM:
-		for (long j = 0; j<(matSize); j++)
-			mat[j] = (float)rand() / (float)RAND_MAX;
+		printf(" ===============\n");
+		for (long j = 0; j < (matSize); j++)
+		{
+			mat[j] = rand() % 10;
+			printf(" == %d \n", mat[j]);
+		}
 		break;
 	case MATRIX_IDENTITY:
 		// NOT IN USE
@@ -68,34 +79,38 @@ float* prepareSquareMatrix(int isz, MATRIX_TYPE typ)
 int main()
 {
 	// Pointer for matrix
-	float *A, *B, *SOL;				// HOST
-	float *cudaA, *cudaB, *cudaSUM, *cudaRET;	// DEVICE
+	int *A, *B, *SOL;				// HOST
+	int *cudaA, *cudaB, *cudaSUM, *cudaRET;	// DEVICE
 	int msize = 3;  // 3X3
 
 	// Initializing matrix with data
 	A = prepareSquareMatrix(msize, MATRIX_RANDOM);  
 	B = prepareSquareMatrix(msize, MATRIX_RANDOM);  
 	SOL = prepareSquareMatrix(msize, MATRIX_INITIALIZE);  
-	cudaRET = (float *)malloc(msize*msize*sizeof(float));
-	memset(cudaRET, 0, msize*msize*sizeof(float));
+	cudaRET = (int *)malloc(msize*msize*sizeof(int));
+	memset(cudaRET, 0, msize*msize*sizeof(int));
 
 	CPUMult(A, B, SOL, msize*msize);
 
-	CUDA_SAFE_CALL(cudaMalloc((void **)&cudaA, msize*msize*sizeof(float)));
-	CUDA_SAFE_CALL(cudaMalloc((void **)&cudaB, msize*msize*sizeof(float)));
-	CUDA_SAFE_CALL(cudaMalloc((void **)&cudaSUM, msize*msize*sizeof(float)));
+	CUDA_SAFE_CALL(cudaMalloc((void **)&cudaA, msize*msize*sizeof(int)));
+	CUDA_SAFE_CALL(cudaMalloc((void **)&cudaB, msize*msize*sizeof(int)));
+	CUDA_SAFE_CALL(cudaMalloc((void **)&cudaSUM, msize*msize*sizeof(int)));
 
 	// we are calculating 3X3 matrix so we will need only 9 threads
 	int threadsPerBlock = 9;
 	int blocksPerGrid = (msize*msize + threadsPerBlock - 1) / threadsPerBlock;
 
-	CUDA_SAFE_CALL(cudaMemcpy(cudaA, A, msize*msize*sizeof(float), cudaMemcpyHostToDevice));
-	CUDA_SAFE_CALL(cudaMemcpy(cudaB, B, msize*msize*sizeof(float), cudaMemcpyHostToDevice));
-	GPUMult << <blocksPerGrid, threadsPerBlock >> >(cudaA, cudaB, cudaSUM, msize*msize);
+	CUDA_SAFE_CALL(cudaMemcpy(cudaA, A, msize*msize*sizeof(int), cudaMemcpyHostToDevice));
+	CUDA_SAFE_CALL(cudaMemcpy(cudaB, B, msize*msize*sizeof(int), cudaMemcpyHostToDevice));
+	dim3 dimGrid(3, 3);
+	dim3 dimBlock(3, 3);
+	//GPUMult << <blocksPerGrid, threadsPerBlock >> >(cudaA, cudaB, cudaSUM, msize);
+	GPUMult << <1, dimBlock >> >(cudaA, cudaB, cudaSUM, msize);
 	_sleep(10);
-	CUDA_SAFE_CALL(cudaMemcpy(cudaRET, cudaSUM, msize*msize*sizeof(float), cudaMemcpyDeviceToHost));
+	CUDA_SAFE_CALL(cudaMemcpy(cudaRET, cudaSUM, msize*msize*sizeof(int), cudaMemcpyDeviceToHost));
 
-
+	/*for (int i = 0; i < 9; i++)
+		printf("\n [%d] =[%d]", i, cudaRET[i]);*/
 	printf("\nBlocks per Grid :%d\nThreads pre Block :%d\n", blocksPerGrid, threadsPerBlock);
 	cudaFree(cudaA);
 	cudaFree(cudaB);
@@ -103,6 +118,7 @@ int main()
 	free(A);
 	free(B);
 	free(SOL);
+	cudaDeviceReset();
 
 	return 0;
 }
